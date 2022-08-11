82fe6d96df4ad6f9dc795571032df9546745b995
rLukoyanov <rulukoyanov@gmail.com>
2022-08-11 21:41:37 +0300

    feat(mainPage): add PP description and fix rating layput bugs

--- components/Main/Rating.jsx
+++ components/Main/Rating.jsx
@@ -8,77 +8,78 @@ import styles from "./Rating.module.scss";
 import protocol from "../../protocol";
 
 export default function Rating({ host = "" }) {
-  const [companies, setCompanies] = useState([
-    {
-      imagePath: "",
-      name: "Татнефть",
-    },
-    {
-      imagePath: "",
-      name: "Татнефть",
-    },
-    {
-      imagePath: "",
-      name: "Татнефть",
-    },
-    {
-      imagePath: "",
-      name: "Татнефть",
-    },
-    {
-      imagePath: "",
-      name: "Татнефть",
-    },
-    {
-      imagePath: "",
-      name: "Татнефть",
-    },
-    {
-      imagePath: "",
-      name: "Татнефть",
-    },
-    {
-      imagePath: "",
-      name: "Татнефть",
-    },
-    {
-      imagePath: "",
-      name: "Татнефть",
-    },
-    {
-      imagePath: "",
-      name: "Татнефть",
-    },
-    {
-      imagePath: "",
-      name: "Татнефть",
-    },
-    {
-      imagePath: "",
-      name: "Татнефть",
-    },
-    {
-      imagePath: "",
-      name: "Татнефть",
-    },
-    {
-      imagePath: "",
-      name: "Татнефть",
-    },
-  ]);
+    const [companies, setCompanies] = useState([
+        {
+            imagePath: "",
+            name: "Татнефть",
+        },
+        {
+            imagePath: "",
+            name: "Татнефть",
+        },
+        {
+            imagePath: "",
+            name: "Татнефть",
+        },
+        {
+            imagePath: "",
+            name: "Татнефть",
+        },
+        {
+            imagePath: "",
+            name: "Татнефть",
+        },
+        {
+            imagePath: "",
+            name: "Татнефть",
+        },
+        {
+            imagePath: "",
+            name: "Татнефть",
+        },
+        {
+            imagePath: "",
+            name: "Татнефть",
+        },
+        {
+            imagePath: "",
+            name: "Татнефть",
+        },
+        {
+            imagePath: "",
+            name: "Татнефть",
+        },
+        {
+            imagePath: "",
+            name: "Татнефть",
+        },
+        {
+            imagePath: "",
+            name: "Татнефть",
+        },
+        {
+            imagePath: "",
+            name: "Татнефть",
+        },
+        {
+            imagePath: "",
+            name: "Татнефть",
+        },
+    ]);
 
-  return (
-    <div className={styles.rating}>
-      <div className={styles.container}>
-        <p className={styles.title}>
-          Рейтинг компаний, принимающих студентов на производственную практику
-        </p>
-        {companies.length > 0 ? (
-          <Companies companies={companies} />
-        ) : (
-          <p>Компаний нет</p>
-        )}
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.rating}>
+            <p className={styles.title}>
+                Рейтинг компаний, принимающих студентов на производственную
+                практику
+            </p>
+            <div className={styles.container}>
+                {companies.length > 0 ? (
+                    <Companies companies={companies} />
+                ) : (
+                    <p>Компаний нет</p>
+                )}
+            </div>
+        </div>
+    );
 }
