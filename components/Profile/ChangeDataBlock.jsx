9b4051fce0a1322a8acb371041e7d18618c465e7
rLukoyanov <rulukoyanov@gmail.com>
2022-08-15 22:22:08 +0300

    feat(profile): update user profile, add new layout

--- components/Profile/ChangeDataBlock.jsx
+++ components/Profile/ChangeDataBlock.jsx
@@ -1,109 +1,127 @@
-import React, { useState, useEffect } from 'react';
-import getMonth from 'date-fns/getMonth';
-import getDaysInMonth from 'date-fns/getDaysInMonth';
+import React, { useState, useEffect } from "react";
+import getMonth from "date-fns/getMonth";
+import getDaysInMonth from "date-fns/getDaysInMonth";
 
-import styles from './profile.module.scss';
-import ProfileSelect from './ProfileSelect';
+import Button from "../Common/Button/Button";
+import ProfileSelect from "./ProfileSelect";
+
+import styles from "./profile.module.scss";
 
 export default function ChangeDataBlock({
-  name = 'userName',
-  surname = 'userSurname',
-  email = 'test@mail.ru',
-  date = '111111111',
-  ed = '',
+    name = "userName",
+    surname = "userSurname",
+    email = "test@mail.ru",
+    date = "111111111",
+    ed = "Среднее образование",
+    lang = ["Родной язык(родной)"],
 }) {
-  const [year] = useState(date[0] + date[1] + date[2] + date[3]);
-  const [month, setMonth] = useState(
-    getMonth(new Date(year, Number(date[4] + date[5] - 1), date[6] + date[7])),
-  );
-  const [day] = useState(date[6] + date[7]);
+    const [year] = useState(date[0] + date[1] + date[2] + date[3]);
+    const [month, setMonth] = useState(
+        getMonth(
+            new Date(year, Number(date[4] + date[5] - 1), date[6] + date[7])
+        )
+    );
+    const [day] = useState(date[6] + date[7]);
 
-  let days = [];
+    let days = [];
 
-  let maxDayInMonth = getDaysInMonth(month);
-  for (let i = 1; i <= maxDayInMonth; i++) {
-    days.push(i);
-  }
-
-  useEffect(() => {
-    maxDayInMonth = getDaysInMonth(month);
-    days = [];
+    let maxDayInMonth = getDaysInMonth(month);
     for (let i = 1; i <= maxDayInMonth; i++) {
-      days.push(i);
+        days.push(i);
     }
-  }, [month, year]);
 
-  const months = [
-    'января',
-    'февраля',
-    'марта',
-    'апреля',
-    'мая',
-    'июня',
-    'июля',
-    'августа',
-    'сентября',
-    'октября',
-    'ноября',
-    'декабря',
-  ];
+    useEffect(() => {
+        maxDayInMonth = getDaysInMonth(month);
+        days = [];
+        for (let i = 1; i <= maxDayInMonth; i++) {
+            days.push(i);
+        }
+    }, [month, year]);
 
-  const DATE = new Date();
+    const months = [
+        "января",
+        "февраля",
+        "марта",
+        "апреля",
+        "мая",
+        "июня",
+        "июля",
+        "августа",
+        "сентября",
+        "октября",
+        "ноября",
+        "декабря",
+    ];
 
-  const years = [];
-  for (let i = 1980; i <= DATE.getFullYear() - 18; i++) {
-    years.push(i);
-  }
+    const DATE = new Date();
 
-  const onMonthChange = (value) => {
-    setMonth(new Date(year, months.indexOf(value), day));
-  };
+    const years = [];
+    for (let i = 1980; i <= DATE.getFullYear() - 18; i++) {
+        years.push(i);
+    }
 
-  const onDayChange = (value) => {
-    setMonth(new Date(year, month, value));
-  };
+    const onMonthChange = (value) => {
+        setMonth(new Date(year, months.indexOf(value), day));
+    };
 
-  const onYearChange = (value) => {
-    setMonth(new Date(year, month, value));
-  };
+    const onDayChange = (value) => {
+        setMonth(new Date(year, month, value));
+    };
 
-  return (
-    <div className={styles.inputBlock}>
-      <label>
-        <span>Имя</span>
-        <input disabled value={name} />
-      </label>
-      <label>
-        <span>Фамилия</span>
-        <input disabled value={surname} />
-      </label>
-      <label>
-        <span>E-mail</span>
-        <input disabled value={email} />
-      </label>
-      <label>
-        <span>Дата рождения</span>
-        <div>
-          <ProfileSelect
-            className={`${styles.customSelect} ${styles.day}`}
-            defaultValue={day}
-            values={days}
-            onChange={onDayChange}
-          />
-          <ProfileSelect
-            className={`${styles.customSelect} ${styles.birth}`}
-            defaultValue={months[month]}
-            values={months}
-            onChange={onMonthChange}
-          />
-          <ProfileSelect
-            className={`${styles.customSelect} ${styles.birth}`}
-            defaultValue={year}
-            values={years}
-            onChange={onYearChange}
-          />
+    const onYearChange = (value) => {
+        setMonth(new Date(year, month, value));
+    };
+
+    return (
+        <div className={styles.inputBlock}>
+            <label>
+                <span>Имя</span>
+                <input value={name} />
+            </label>
+            <label>
+                <span>Фамилия</span>
+                <input value={surname} />
+            </label>
+            <label>
+                <span>E-mail</span>
+                <input value={email} />
+            </label>
+            <label>
+                <span>Дата рождения</span>
+                <div>
+                    <ProfileSelect
+                        className={`${styles.customSelect} ${styles.day}`}
+                        defaultValue={day}
+                        values={days}
+                        onChange={onDayChange}
+                    />
+                    <ProfileSelect
+                        className={`${styles.customSelect} ${styles.birth}`}
+                        defaultValue={months[month]}
+                        values={months}
+                        onChange={onMonthChange}
+                    />
+                    <ProfileSelect
+                        className={`${styles.customSelect} ${styles.birth}`}
+                        defaultValue={year}
+                        values={years}
+                        onChange={onYearChange}
+                    />
+                </div>
+            </label>
+            <label>
+                <span>Образование</span>
+                <input value={ed} />
+            </label>
+            <label>
+                <span>Знание языков</span>
+                <div className={styles.column}>
+                    {lang.map((item, index) => (
+                        <input value={lang[index]} />
+                    ))}
+                </div>
+            </label>
+            <Button className={styles.saveBtn} disabled>Сохранить</Button>
         </div>
-      </label>
-    </div>
-  );
+    );
 }
--- components/Profile/ProfileSelect.jsx
+++ components/Profile/ProfileSelect.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import { Select } from 'antd';
+import React from "react";
+import { Select } from "antd";
 
 const { Option } = Select;
 
 export default function ProfileSelect({
-  defaultValue = '',
-  className = '',
-  values = [],
-  onChange = () => {},
+    defaultValue = "",
+    className = "",
+    values = [],
+    onChange = () => {},
 }) {
-  return (
-    <>
-      <Select
-        autoFocus={false}
-        defaultOpen={false}
-        defaultValue={defaultValue}
-        className={className}
-        onChange={onChange}
-      >
-        {values.map((item) => (
-          <Option key={item} value={item}>
-            {item}
-          </Option>
-        ))}
-      </Select>
-    </>
-  );
+    return (
+        <>
+            <Select
+                autoFocus={false}
+                defaultOpen={false}
+                defaultValue={defaultValue}
+                className={className}
+                onChange={onChange}
+            >
+                {values.map((item) => (
+                    <Option key={item} value={item}>
+                        {item}
+                    </Option>
+                ))}
+            </Select>
+        </>
+    );
 }
