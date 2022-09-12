d02580f3ea7725f4f6888a895636a08bb014e479
rLukoyanov <rulukoyanov@gmail.com>
2022-09-12 12:54:23 +0400

    feat(fetchReg): add registration

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -7,6 +7,7 @@ import Button from "../../components/Common/Button/Button";
 import AuthContext from "../../store/auth-context";
 
 import styles from "../../components/Login/Login.module.scss";
+import axios from "axios";
 
 export default function LoginPage() {
     const logger = pino({
@@ -14,6 +15,7 @@ export default function LoginPage() {
     });
 
     const [reg, setReg] = useState(false);
+    const [isReg, setIsReg] = useState(false);
 
     const [enteredLogin, setEnteredLogin] = useState("");
     const [enteredPassword, setEnteredPassword] = useState("");
@@ -33,7 +35,16 @@ export default function LoginPage() {
 
     useEffect(() => {
         setLoading(false);
-    }, [enteredLogin, enteredPassword]);
+    }, [
+        enteredLogin,
+        enteredPassword,
+        enteredEducation,
+        enteredEmail,
+        enteredNumber,
+        enteredBirthday,
+        enteredName,
+        enteredSurname,
+    ]);
 
     const toggleReg = () => {
         setReg(!reg);
@@ -88,16 +99,32 @@ export default function LoginPage() {
         }
     };
 
-    const onRegister = () => {
-        console.group("mainUserData");
-        console.log("name:", enteredName);
-        console.log("surname:", enteredSurname);
-        console.log("password:", enteredPassword);
-        console.group("secondaryUserData");
-        console.log("birthday:", enteredBirthday);
-        console.log("phoneNumber:", enteredNumber);
-        console.log("email:", enteredEmail);
-        console.log("education:", enteredEducation);
+    const onRegister = async () => {
+        const year =
+            enteredBirthday[0] +
+            enteredBirthday[1] +
+            enteredBirthday[2] +
+            enteredBirthday[3];
+        const mounth = enteredBirthday[5] + enteredBirthday[6];
+        const day = enteredBirthday[8] + enteredBirthday[9];
+        const refactoringDate = `${day}/${mounth}/${year}`;
+        try {
+            const { data } = await axios.post("/api/user/createUser", {
+                enteredName,
+                enteredSurname,
+                enteredPassword,
+                enteredNumber,
+                enteredBirthday: refactoringDate,
+                enteredEmail,
+                enteredEducation,
+            });
+            if (data === "Учётная запись была создана") {
+                setIsReg(true);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+        setLoading(true);
     };
 
     return (
@@ -176,6 +203,7 @@ export default function LoginPage() {
                         className={styles.input}
                     />
                     {reg ?? <p className={styles.error}>{error}</p>}
+                    {setIsReg ?? <p>Вы успешно зарегистрировались</p>}
                     <div className={styles.checkboxPassword}>
                         <label className={styles.checkText}>
                             <Checkbox />
